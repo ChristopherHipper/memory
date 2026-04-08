93cac2ab4571cab9681a1f5cfa572ee1c7e35f88
Christopher Hipper <hipper.christopher@gmail.com>
2026-04-08 12:52:12 +0200

    Add game settings management and display functionality

--- src/scripts/settings.ts
+++ src/scripts/settings.ts
@@ -20,3 +20,65 @@ function init(): void {
 
 
 }
+
+
+document.addEventListener("DOMContentLoaded", init)
+const previewSettings = document.getElementById('settings')
+const previewImg = document.getElementById('img')
+
+let gameSettings = {
+    theme: "Code vibes theme",
+    player: "Blue",
+    field: "16 cards",
+}
+
+
+function init() {
+    const form = document.querySelector("form");
+    if (form) {
+        form.addEventListener('click', (e) => { selectSettings(e) })
+    }
+    previewSettings.innerHTML = displaySetting();
+}
+
+function selectSettings(e) {
+    const label = (e.target).closest("label");
+    const fieldset = label.parentElement;
+    const activeImges = fieldset.querySelectorAll(".active")
+    activeImges.forEach(img => {
+        img.classList.remove('active')
+    });
+    const img = label.querySelector(".line")
+    if (img) {
+        img.classList.add('active');
+    }
+    setGameSetting(label);
+}
+
+function setGameSetting(label) {
+    const value = label.innerText;
+    const listId = label.parentElement.id;
+    if (listId === "field") {
+        gameSettings.field = value;
+    } else if (listId === "theme") {
+        gameSettings.theme = value;
+    } else if (listId === "player"){
+        gameSettings.player = value
+    } else {
+        return
+    }
+    previewSettings.innerHTML = displaySetting()
+}
+
+function displaySetting() {
+    return `         <p>${gameSettings.theme}</p>
+                    <div>/</div>
+                    <p>${gameSettings.player}</p>
+                    <div>/</div>
+                    <p>${gameSettings.field}</p>
+                    <div>/</div>
+                    <button id="start-game-btn" class="start-game-btn play-btn">
+                        Start
+                    </button>
+                `
+}
