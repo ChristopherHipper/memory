4fc4d62ec2d34bc2e0a0d247e797c86ead60049f
Christopher Hipper <hipper.christopher@gmail.com>
2026-04-13 20:57:50 +0200

    start design themes

--- src/pages/gameTemplate.ts
+++ src/pages/gameTemplate.ts
@@ -1,25 +1,28 @@
 import { Game } from "../scripts/models/game.class";
 
 export function renderMain(game:Game):string {
-    return `<header>
-                <div class="players-wrapper">
-                    <div>
-                        <img src="../../assets/img/${game.gameTheme}-theme/${game.chosenPlayer}.png" alt="">
-                        <p>${game.chosenPlayerPoints}</p>
+    return `<main class="game" data-theme="${game.gameTheme}">
+                <header>
+                    <div class="players-wrapper">
+                        <div>
+                            <img src="../../assets/img/${game.gameTheme}-theme/${game.chosenPlayer}.png" alt="">
+                            <p class="${game.chosenPlayer}">${game.chosenPlayerPoints}</p>
+                        </div>
+                        <div>
+                            <img src="../../assets/img/${game.gameTheme}-theme/${game.opponent}.png" alt="">
+                            <p class="${game.opponent}">${game.opponentPoints}</p>
+                        </div>
                     </div>
-                    <div>
-                        <img src="../../assets/img/${game.gameTheme}-theme/${game.opponent}.png" alt="">
-                        <p>${game.opponentPoints}</p>
+                    <div class="current-player">
+                        <p>Current player:</p>
+                        <img src="../../assets/img/${game.gameTheme}-theme/${game.currentPlayer}.png" alt="">
                     </div>
-                </div>
-                <div class="current-player">
-                    <p>Current Player:</p>
-                    <img src="../../assets/img/${game.gameTheme}-theme/${game.currentPlayer}.png" alt="">
-                </div>
-                <button class="exit-btn">
-                    <img src="../../assets/img//exit.png" alt="">
-                    Exit Game
-                </button>
-            </header>`
+                    <button class="button exit-btn">
+                        <img src="../../assets/img//exit.png" alt="">
+                        Exit game
+                    </button>
+                </header>
+            </main>    
+                `
     
 }
